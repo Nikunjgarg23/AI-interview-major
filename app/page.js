b4bc22e66bf937c204297664e5013848c7cf4892
Nikunj <23nikunjgarg@gmail.com>
2025-01-11 18:05:36 +0530

    updated face emotion

--- app/page.js
+++ app/page.js
@@ -8,8 +8,7 @@ export default function Home() {
   const router = useRouter();
   useEffect(()=>{
     router.replace('/dashboard');
-    
-  },[]);
+  },[router]);
   return (
   <div className="text-center">
     <h1>Please Wait!!</h1>
